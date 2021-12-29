1556b8f27d4b3bdf583cc0e1c3271706bbf529a6
ARAVINDTECHEFOLKS <96331247+ARAVINDTECHEFOLKS@users.noreply.github.com>
2021-12-29 11:23:40 +0530

    Update cartscript.js

--- cartscript.js
+++ cartscript.js
@@ -1,119 +1,237 @@
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="styles.css" />
-  </head>
-  <body style="background-color: rgb(240, 236, 236)">
-    <nav>
-      <div class="nav-belt">
-        <div class="row">
-          <div class="col-3 nav-left">
-            <a class="anchor" href="#">
-              <img src="./assets/logoamazon1.jpg" width="90px" height="50px" />
-            </a>
-            <a class="anchor" href="#">Select your address</a>
-          </div>
-          <div class="nav-fill col-5">
-            <div class="input-group">
-              <span class="input-group-text">All</span>
-              <input type="text" class="w-75" name="search" />
-              <button class="btn btn-warning" type="search">
-                <i class="fa fa-search"></i>
-              </button>
-            </div>
-          </div>
+console.log("running");
+var repeated = [];
+var n = 0;
+let carts = document.querySelectorAll(".add-cart");
+let products = [
+  {
+    name: "Lenovo Tab M10 HD 2nd Gen",
+    tag: "Lenovo Tab M10",
+    price: 11294,
+    inCart: 0,
+  },
+  {
+    name: "Lenovo Tab M10 FHD Plus Tablet",
+    tag: "Lenovo Tab M10 FHD",
+    price: 18990,
+    inCart: 0,
+  },
+  {
+    name: "Lenovo Yoga Smart Tablet",
+    tag: "Lenovo Yoga Smart Tablet",
+    price: 19999,
+    inCart: 0,
+  },
+  {
+    name: "Lenovo Tab M8 HD 2nd Gen",
+    tag: "Lenovo Tab M8",
+    price: 10890,
+    inCart: 0,
+  },
+  {
+    name: "Lenovo Tab M7 3rd Gen",
+    tag: "Lenovo Tab M7",
+    price: 7999,
+    inCart: 0,
+  },
+  {
+    name: "Lenovo Tab M10 HD Tablet",
+    tag: "Lenovo Tab M10 HD",
+    price: 10449,
+    inCart: 0,
+  },
+];
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
+}
+function onLoadCartNumbers() {
+  let productNumbers = sessionStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+}
+function cartNumbers(product) {
+  let productNumbers = sessionStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
 
-          <div class="col-4">
-            <ul class="nav-right">
-              <li>
-                <a class="anchor" href="#">Accounts &amp;lists</a>
-              </li>
-              <li>
-                <a class="anchor" href="#">Returns &amp; orders</a>
-              </li>
-              <li>
-                <!-- <a class="anchor" href="#"><img src="/assets/cart.jpg" width=30px height=30px>Carts</a> -->
-                <button
-                  class="cart btn btn-primary"
-                  onclick="window.location.href='cart.html'"
-                >
-                  <i class="fa fa-shopping-cart" style="font-size: 36px"></i
-                  >(<span>0</span>)
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="nav-main">
-        <div class="row">
-          <section class="col-9">
-            <ul class="nav-mainleft">
-              <li><a class="anchor" href="#">All</a></li>
-              <li><a class="anchor" href="#">Best Sellers</a></li>
-              <li><a class="anchor" href="#">Mobiles</a></li>
-              <li><a class="anchor" href="#">Customer Services</a></li>
-              <li><a class="anchor" href="#">Electronics</a></li>
-              <li><a class="anchor" href="#">Fashion</a></li>
-              <li><a class="anchor" href="#">Today's Deals</a></li>
-              <li><a class="anchor" href="#">Home & Kitchen</a></li>
-              <li><a class="anchor" href="#">Amazon Pay</a></li>
-              <li><a class="anchor" href="#">Computers</a></li>
-              <li><a class="anchor" href="#">New Releases</a></li>
-            </ul>
-          </section>
-          <section class="col-3">
-            <a class="nav-logo" href="#">
-              <img
-                src="D:\javascript_programs\Cart Demo\cart\assets\amazon_arrow_logo.jpg"
-                width="25px"
-                height="25px"
-                style="border-radius: 5px"
-              />
-              Shop made easy| Download app</a
-            >
-          </section>
-        </div>
-      </div>
-    </nav>
-    <div
-      class="products-container table_margin"
-      style="background-color: floralwhite"
-    >
+  if (productNumbers) {
+    if (product.tag === repeated[n]) {
+      alert("this item is already added");
+    } else {
+      var check = product.tag;
+      let result = repeated.every(function (element) {
+        return repeated.includes(check);
+      });
+      if (result === true) {
+        alert("this item is already added");
+      } else {
+        sessionStorage.setItem("cartNumbers", productNumbers + 1);
+        document.querySelector(".cart span").textContent = productNumbers + 1;
+        repeated[n] = product.tag;
+        n++;
+        setItems(product);
+      }
+    }
+  } else {
+    repeated[n] = product.tag;
+    n++;
+    sessionStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+    setItems(product);
+  }
+}
+function setItems(product) {
+  console.log("my product is", product);
+  let cartItems = sessionStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+  //console.log("my cartItems", cartItems);
+  sessionStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  //console.log("stringify is", cartItems);
+}
+function totalCost(product) {
+  //console.log("price of product is ", product.price);
+  let cartCost = sessionStorage.getItem("totalCost");
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    sessionStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    sessionStorage.setItem("totalCost", product.price);
+  }
+}
+var k = 0;
+function displayCart() {
+  var total = 0;
+  let inCartCount = 0;
+
+  let cartItems = sessionStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let totalPrice = document.querySelector(".total-price");
+  let productsContainer = document.querySelector(".products");
+  if (cartItems && productsContainer) {
+    productsContainer.innerHTML = ``;
+    totalPrice.innerHTML += ``;
+    Object.values(cartItems).map((item) => {
+      productsContainer.innerHTML += `
       <div class="row">
-        <div class="col"><h3>Shopping Cart</h3></div>
-        <div class="col-6"></div>
-        <div class="col"><h5>Price</h5></div>
-      </div>
-      <hr />
-      <div>
-        <div class="products">
-          <h5>Your cart is empty</h5>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div class="col"><h3></h3></div>
-        <div class="col-4"></div>
-        <div class="total-price col"></div>
-      </div>
-    </div>
-    <script src="cartscript.js"></script>
-  </body>
-</html>
+      <div class="col image_col">
+              <img src="./assets/${item.tag}.jpg">
+            </div>
+            <div class="col-6">
+            <h5>${item.name}</h5>
+                <table >
+                  <tr>
+                    <td>
+                      <button class="dBtn"><</button></i><span id="checkingValue"><a class="value" >${item.inCart}</a></span><button class="iBtn">></botton>
+                    </td>
+                    <td style="padding :20px"><a href="#">Delete</a></td>
+                    <td style="padding :20px"><a href="#">see more like this</a></td>
+                  </tr>
+                </table>   
+            </div>
+            <div class="col">
+             <h5>${item.price}</h5>
+            </div>
+            </div>`;
+      total += item.inCart * item.price;
+      inCartCount = item.inCart;
+
+      console.log(total);
+      // let decrease = document.querySelectorAll(".dBtn");
+      // for (var i = 0; i < decrease.length; i++) {
+      //   decrease[i].onclick = function decreaseValue() {
+      //     num = num - 1;
+      //     document.getElementById("value").innerHTML = num;
+      //   };
+      // }
+      // let increase = document.querySelectorAll(".iBtn");
+      // for (var i = 0; i < increase.length; i++) {
+      //   increase[i].onclick = function decreaseValue() {
+      //     num = num + 1;
+      //     document.getElementById("value").innerHTML = num;
+      //   };
+      // }
+
+      // console.log(k);
+    });
+    Object.values(cartItems).map((item) => {
+      let decrease = document.querySelectorAll(".dBtn");
+      let valueD = document.querySelectorAll(".value");
+
+      for (let j = 0; j < decrease.length; j++) {
+        let num = inCartCount;
+        var cartNumPass2;
+        var outNum = num;
+
+        decrease[j].onclick = function decreaseValue() {
+          if (cartNumPass2 != null && cartNumPass2 != undefined) {
+            console.log("you are here");
+            if (cartNumPass2 === outNum + 1) {
+              let cartNumD = outNum;
+              var numOfBlock = cartNumD;
+            } else {
+              let cartNumD = cartNumPass2;
+              var numOfBlock = cartNumD;
+            }
+          } else {
+            console.log(" why you are here");
+            let cartNumD = num;
+            var numOfBlock = cartNumD;
+          }
+          console.log("decrease" + j);
+          numOfBlock = numOfBlock - 1;
+          valueD[j].innerHTML = numOfBlock;
+          outNum = numOfBlock;
+        };
+        var cartnumPass = outNum;
+      }
+      let increase = document.querySelectorAll(".iBtn");
+      //let valueI = document.querySelectorAll(".value");
+      for (let j = 0; j < increase.length; j++) {
+        let cartNumI = cartnumPass;
+
+        increase[j].onclick = function increaseValue() {
+          console.log("increase" + j);
+          cartNumI = cartNumI + 1;
+          valueD[j].innerHTML = cartNumI;
+          cartNumPass2 = cartNumI;
+        };
+      }
+      k++;
+      console.log(k);
+    });
+
+    console.log(total);
+
+    Object.values(cartItems).map((item) => {
+      totalPrice.innerHTML = `
+ 
+    <h5>TotalPrice: ${total}</h5>`;
+    });
+  }
+  console.log("hai");
+}
+onLoadCartNumbers();
+displayCart();
+
+for (var i = 0; i < repeated.length; i++) {
+  console.log(repeated[i]);
+}
+console.log("last");
+
