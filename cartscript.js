a16e0c45e66dcc3ee56788a935d4c691e525b966
Aravind Thodeti <you@example.com>
2021-12-30 11:13:23 +0530

    push-1

--- cartscript.js
+++ cartscript.js
@@ -95,6 +95,7 @@ function setItems(product) {
       };
     }
     cartItems[product.tag].inCart += 1;
+    console.log("inCart", product.inCart);
   } else {
     product.inCart = 1;
     cartItems = {
@@ -110,9 +111,13 @@ function totalCost(product) {
   let cartCost = sessionStorage.getItem("totalCost");
   if (cartCost != null) {
     cartCost = parseInt(cartCost);
-    sessionStorage.setItem("totalCost", cartCost + product.price);
+    let totalCostofItem = product.inCart * product.price;
+    console.log("productincart is ", product.inCart);
+    console.log("productPrice is ", product.price);
+    sessionStorage.setItem("totalCost", cartCost + totalCostofItem);
   } else {
-    sessionStorage.setItem("totalCost", product.price);
+    let totalCostofItem = product.inCart * product.price;
+    sessionStorage.setItem("totalCost", totalCostofItem);
   }
 }
 var k = 0;
@@ -130,23 +135,19 @@ function displayCart() {
     Object.values(cartItems).map((item) => {
       productsContainer.innerHTML += `
       <div class="row">
-      <div class="col image_col">
+      <div class="image col image_col">
               <img src="./assets/${item.tag}.jpg">
             </div>
-            <div class="col-6">
-            <h5>${item.name}</h5>
-                <table >
-                  <tr>
-                    <td>
-                      <button class="dBtn"><</button></i><span id="checkingValue"><a class="value" >${item.inCart}</a></span><button class="iBtn">></botton>
-                    </td>
-                    <td style="padding :20px"><a href="#">Delete</a></td>
-                    <td style="padding :20px"><a href="#">see more like this</a></td>
-                  </tr>
-                </table>   
+            <div class="text col-6">
+            <h5 class="itemName">${item.name}</h5>
+            <div class="table">
+                      <button class="dBtn"><</button></i><span id="checkingValue"><a class="value" >${item.inCart}</a></span><button class="iBtn">></button>
+                    <a class="remove" href="#">Delete</a> <a> | </a>
+                  <a href="#">see more like this</a>
+                </div> 
             </div>
             <div class="col">
-             <h5>${item.price}</h5>
+             <h5 class="price">${item.price}</h5>
             </div>
             </div>`;
       total += item.inCart * item.price;
@@ -154,49 +155,142 @@ function displayCart() {
 
       console.log(total);
     });
-    Object.values(cartItems).map((item) => {
-      let decrease = document.querySelectorAll(".dBtn");
-      let valueD = document.querySelectorAll(".value");
-      let num = new Array(decrease.length);
-      num.fill(inCartCount);
-
-      for (let j = 0; j < decrease.length; j++) {
-        decrease[j].onclick = function decreaseValue() {
-          console.log("decrease" + j);
-          let numD = num[j];
-          numD = numD - 1;
-          valueD[j].innerHTML = numD;
-          num[j] = numD;
-          if (numD < 1) {
-            num[j] = 1;
-            valueD[j].innerHTML = num[j];
-          }
-        };
+    //
+    let decrease = document.querySelectorAll(".dBtn");
+    let valueD = document.querySelectorAll(".value");
+    let num = new Array(decrease.length);
+    num.fill(inCartCount);
+    // let priceValue = document.querySelectorAll(".price").innerText
+    var totalCountOfAmount = 0;
+    let priceArray = new Array(decrease.length);
+    for (let i = 0; i < priceArray.length; i++) {
+      priceArray[i] = document.getElementsByClassName("price")[i].innerText;
+      priceArray[i] = parseInt(priceArray[i]);
+      //totalCountOfAmount += totalOfPriceArray[i];
+    }
+    var totalOfPriceArray = priceArray.slice();
+    function calculate() {
+      totalCountOfAmount = 0;
+      for (let i = 0; i < totalOfPriceArray.length; i++) {
+        totalCountOfAmount += totalOfPriceArray[i];
       }
-      let increase = document.querySelectorAll(".iBtn");
-      for (let j = 0; j < increase.length; j++) {
-        increase[j].onclick = function increaseValue() {
-          console.log("increase" + j);
-          let numI = num[j];
-          numI = numI + 1;
-          valueD[j].innerHTML = numI;
-          num[j] = numI;
-          if (numI < 1) {
-            num[j] = 1;
-            valueD[j].innerHTML = num[j];
-          }
-        };
-      }
-      k++;
-      console.log(k);
-    });
+    }
+    calculate();
+    console.log("totalAmount", totalCountOfAmount);
 
+    for (let j = 0; j < decrease.length; j++) {
+      decrease[j].onclick = function decreaseValue() {
+        let finalAmount = totalCountOfAmount;
+        console.log("finalAmount", finalAmount);
+        priceValue = parseInt(priceArray[j]);
+        console.log("decrease" + j);
+        console.log(priceValue, typeof priceValue);
+        let numD = num[j];
+        numD = numD - 1;
+        valueD[j].innerHTML = numD;
+        num[j] = numD;
+        if (numD < 1) {
+          num[j] = 1;
+          valueD[j].innerHTML = num[j];
+        }
+        let totalOfPrice = num[j] * priceValue;
+        totalOfPriceArray[j] = totalOfPrice;
+        calculate();
+        console.log(totalCountOfAmount);
+        document.getElementsByClassName("price")[j].innerHTML = totalOfPrice;
+        var totalNum = document.getElementsByClassName("total")[0].innerText;
+        totalNum = parseInt(totalNum);
+        finalAmount = finalAmount - priceValue;
+        document.getElementsByClassName("total")[0].innerHTML =
+          totalCountOfAmount;
+        // let elementName =
+        //   document.getElementsByClassName("itemName")[j].innerText;
+        // console.log(elementName);
+        // for (let i = 0; i < products.length; i++) {
+        //   if (products[i].name === elementName) {
+        //     cartNumbers(products[i]);
+        //     totalCost(products[i]);
+        //     break;
+        //   }
+        // }
+      };
+    }
+
+    let increase = document.querySelectorAll(".iBtn");
+    for (let j = 0; j < increase.length; j++) {
+      increase[j].onclick = function increaseValue() {
+        let finalAmount = totalCountOfAmount;
+        console.log("finalAmount", finalAmount);
+        console.log("increase" + j);
+        priceValue = parseInt(priceArray[j]);
+        console.log(priceValue, typeof priceValue);
+        let numI = num[j];
+        numI = numI + 1;
+        valueD[j].innerHTML = numI;
+        num[j] = numI;
+        if (numI < 1) {
+          num[j] = 1;
+          valueD[j].innerHTML = num[j];
+        }
+        let totalOfPrice = num[j] * priceValue;
+        totalOfPriceArray[j] = totalOfPrice;
+        calculate();
+        console.log(totalCountOfAmount);
+        document.getElementsByClassName("price")[j].innerHTML = totalOfPrice;
+        var totalNum = document.getElementsByClassName("total")[0].innerText;
+        totalNum = parseInt(totalNum);
+        finalAmount = finalAmount - priceValue;
+        document.getElementsByClassName("total")[0].innerHTML =
+          totalCountOfAmount;
+        // let elementName =
+        //   document.getElementsByClassName("itemName")[j].innerText;
+        // console.log(elementName);
+        // for (let i = 0; i < products.length; i++) {
+        //   if (products[i].name === elementName) {
+        //     cartNumbers(products[i]);
+        //     totalCost(products[i]);
+        //     break;
+        //   }
+        // }
+      };
+    }
+    k++;
+    //console.log(k);
+
+    //Object.values(cartItems).map((item) => {
+    let removeButton = document.querySelectorAll(".remove");
+    for (let i = 0; i < removeButton.length; i++) {
+      var button = removeButton[i];
+      button.addEventListener("click", function (event) {
+        var buttonClicked = event.target;
+        var price = document.getElementsByClassName("price")[0].innerText;
+        console.log(price);
+        price = parseInt(price);
+        console.log("this is price", price, typeof price);
+        buttonClicked.parentElement.parentElement.parentElement.remove();
+        var totalNum = document.getElementsByClassName("total")[0].innerText;
+        console.log(totalNum, typeof totalNum);
+        //total = total - price;
+        totalNum = parseInt(totalNum);
+        console.log("this is total", totalNum, typeof totalNum);
+        document.getElementsByClassName("total")[0].innerHTML =
+          totalNum - totalOfPriceArray[i];
+        //sessionStorage.removeItem('key');
+      });
+    }
+    console.log(removeButton);
+    //});
     console.log(total);
 
     Object.values(cartItems).map((item) => {
       totalPrice.innerHTML = `
  
-    <h5>TotalPrice: ${total}</h5>`;
+    <table>
+    <tr>
+    <td><h5>TotalPrice: </h5> </td>
+    <td><h5 class="total">${total}</h5></td>
+    </tr>
+    </table>`;
     });
   }
   console.log("hai");
@@ -208,4 +302,6 @@ for (var i = 0; i < repeated.length; i++) {
   console.log(repeated[i]);
 }
 console.log("last");
-
+// if (performance.navigation.type === 1) {
+//   sessionStorage.clear();
+// }
